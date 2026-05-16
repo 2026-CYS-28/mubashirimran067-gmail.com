a3ee39f635edd492204ffe39e717620babed24b1
2026-CYS-28 <abdullahramay577@gmail.com>
2026-05-16 22:30:26 +0500

    Add files via upload

---
+++ PF LAB WEEK 12.py
@@ -0,0 +1,81 @@
+# s={1,2,3,3}
+# print(s)
+
+#task 2
+# s=()
+# print(type(s))
+
+#task 3
+# s={}
+# print(type(s))
+
+#task 4
+# a= {1,2,3}
+# print(a)
+# print(type(a))
+
+# task 5
+# s=("CS","CE","DS","CIVIL ENG.","MECH","ELECT","IBM")
+# for i in s:
+#     print(i)
+
+#task 6
+# s={"CS","CE","DS","CIVIL ENG.","MECH","ELECT","IBM"}
+# s.add("AE")
+# s.remove("CE")
+# s.discard("CIVIL")
+# s.pop()
+# s.clear()
+# print(s)
+
+#task 7
+# s1={"abdullah","hamza","shafitp","obaid"}
+# s2={48,46,45,47}
+# s3={12,13,14,15,16}
+# z=s1.union(s2,s3)
+# print(z)
+
+#task 8
+# s1={"abdullah","hamza","shafitp","obaid"}
+# s2={48,46,45,47}
+# s3={12,13,14,15,16}
+# s1.update(s2)
+# print(s1)
+# print(s2)
+# z=s1.intersection(s2)
+# print(z)
+# print(type(z))
+
+#task 9
+# s1={"abdullah","hamza","shafitp","obaid",67}
+# s2={48,46,45,47,67}
+# s1.intersection_update(s2,s3)
+# s1.discard(13)
+# s2.discard(13)
+# print(s1)
+# print(s2)
+
+#task10
+# s1={"abdullah","hamza","shafitp","obaid",67}
+# s2={48,46,45,47,67}
+# s=s1.symmetric_difference(s2)
+# print(s)
+# print(s1)
+# print(s2)
+
+#taask 11
+# s1={"abdullah","hamza","shafitp","obaid",67}
+# s2={48,46,45,47,67}
+# s=s1.difference(s2)
+# print(s)
+
+#task 12
+ # s1={"abdullah","hamza","shafitp","obaid",67}
+ # s2={48,46,45,47,67}
+ # s=s1.isdisjoint(s2)
+ # print(s)
+
+#task 13
+# s1={1,2,3,4}
+# s2={1,2,3}
+# s2.issuperset(s1)
